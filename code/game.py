f167481279ace68945bb4f82bdef43f13a5b1b63
Leonardo de Almeida <leonardodealmeidagomes83@gmail.com>
2025-07-07 23:51:06 -0300

    Creating asset folder inserting all assets Creating the menu: backgtund, sound and fonts Creating the const file

--- code/game.py
+++ code/game.py
@@ -2,13 +2,14 @@
 # -*- coding: utf-8 -*-
 import pygame
 
-from code.Menu import Menu
+from code.Const import WIN_WIDTH, WINDOW_HEIGHT
+from code.menu import Menu
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 480))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WINDOW_HEIGHT))
 
     def run(self):
         while True:
@@ -16,8 +17,3 @@ class Game:
             menu.run()
             pass
 
-        # Check for all events
-        # for event in pygame.event.get():
-        #     if event.type == pygame.QUIT:
-        #         pygame.quit()
-        #         quit()
--- code/menu.py
+++ code/menu.py
@@ -1,9 +1,41 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame.image
+from pygame import Surface, Rect
+from pygame.font import Font
+
+from code.Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE
+
 
 class Menu:
-    def __init__(self):
-        self.window = None
-
-    def run(self,window ):
+    def __init__(self, window):
         self.window = window
+        self.surf = pygame.image.load('./assets/10.png')
+        self.rect = self.surf.get_rect()
+        self.rect.left = 0
+        self.rect.top = 0
+
+    def run(self, ):
+        pygame.mixer_music.load('./assets/789410__neko_4444__tranquilidad-aventura-quiet-adventure-game.wav')
+        pygame.mixer_music.play(-1)
+        while True:
+            self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(50, "Cities", COLOR_ORANGE, ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, "Shooter", COLOR_ORANGE, ((WIN_WIDTH / 2), 120))
+
+            for i in range(len(MENU_OPTION)):
+                self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
+
+            pygame.display.flip()
+
+            # Check for all events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf = text_font.render(text, True, text_color).convert_alpha()
+        text_rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(text_surf, text_rect)
